993324b06754c38d517c8566aaee31b29a6eb491
Sam Jones <samejones2018@gmail.com>
2023-03-06 18:26:15 -0800

    created the function to save text and id to local storage and made local storage load after html adding in saved text

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,14 +2,35 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+
+// pull data from local storage and apply it to the hour it was saved to
+
+  $(document).ready(function() {
+    
+    $('.time-block').each(function() {
   
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+      let timeBlockId = $(this).attr('id');
+      let savedData = localStorage.getItem(timeBlockId);
+ 
+      if (savedData) {
+        $(this).find('textarea').val(savedData);
+      }
+    });
+  });
+  
+
+  const $saveButton = $('.saveBtn')
+
+  // TODO: Add a listener for click events on the save button. This code should
+  $saveButton.on('click', function (event) {
+    event.preventDefault();
+
+  const $timeBlock = $(this).parent();
+  const $timeBlockId = $timeBlock.attr('id');
+  const $description = $timeBlock.children('textarea').val();
+
+  localStorage.setItem($timeBlockId, $description);
+  });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
